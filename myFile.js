a5a936d3fa30cefc2db8079941b7c8a2c88134c4
HuxleyHedges <75944088+HuxleyHedges@users.noreply.github.com>
2020-12-22 13:12:22 +0000

    Update myFile.js

--- myFile.js
+++ myFile.js
@@ -15,7 +15,7 @@
         var slides = 
             document.getElementsByClassName("mySlides");
         var dots=
-            document.getElementByClassName("dot");
+            document.getElementsByClassName("dot");
         if (n > slides.length) {slideIndex = 1}
         if ( n < 1) {slideIndex = slides.length}
         for (i = 0; i < slides.length; i++) {slides[i].style.display = "none"; }
